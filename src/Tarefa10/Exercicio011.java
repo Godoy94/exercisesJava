aa964f0c2461146d78b43f79be6016c78fb36dfd
Gustavo Godoy Domingues <gustavogodoyd@brq.com>
2022-12-06 01:12:21 -0300

    upload

--- src/Tarefa10/Exercicio011.java
+++ src/Tarefa10/Exercicio011.java
@@ -4,21 +4,25 @@ import java.util.*;
 
 public class Exercicio011 {
     public static void main(String[] args) {
-        /*
-         * 11) Elaborar um programa que apresente como resultado o valor do fatorial dos
-         * valores ímpares situados na faixa numérica de 1 a 10.
-         */
-        Scanner scanner = new Scanner(System.in);
-        int i, n, fat;
+        try (/*
+              * 11) Elaborar um programa que apresente como resultado o valor do fatorial dos
+              * valores ímpares situados na faixa numérica de 1 a 10.
+              */
+                Scanner scanner = new Scanner(System.in)) {
+            int i;
+            int n;
+            int fat;
 
-        System.out.printf("Informe um numero: ");
-        n = scanner.nextInt();
+            System.out.print("Informe um numero");
+            n = scanner.nextInt();
 
-        fat = 1;
-        for (i = 1; i <= n; i++) {
-            fat = fat * i;
+            fat = 1;
+            for (i = 1; i < n; i++) {
+                fat = fat * i;
+            }
+            System.out.printf("\nO fatorial de %d eh igual a %d.\n", n, fat);
+
+            scanner.close();
         }
-        System.out.printf("\nO fatorial de %d eh igual a %d.\n", n, fat);
-        scanner.close();
     }
 }
